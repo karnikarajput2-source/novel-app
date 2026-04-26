5e92d8a0914bb08be05808deaa98930c58d4c1c3
Karnika Rajput <karnikarajput2@gmail.com>
2026-04-26 21:00:10 +0530

    add manifest for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+  <html
+    lang="en"
+    className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+  >
+    <head>
+      <link rel="manifest" href="/manifest.json" />
+    </head>
+
+    <body className="min-h-full flex flex-col">{children}</body>
+  </html>
+);
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+const nextConfig: NextConfig = withPWA({
+  reactStrictMode: true,
+});
 
 export default nextConfig;
